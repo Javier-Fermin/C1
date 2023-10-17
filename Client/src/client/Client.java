7c27d4dfac2f65842f9b2dd17e63640240446cd7
Fran de Ysasi <fdeysasigonzalez@gmail.com>
2023-10-17 14:23:56 +0200

    First commit Client and Lib

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -5,17 +5,34 @@
  */
 package client;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import view.SignInController;
+
 /**
  *
  * @author javie
  */
-public class Client {
+public class Client extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SignInWindowFXML.fxml"));
+        Parent root = (Parent)loader.load();
+        
+        SignInController cont = ((SignInController)loader.getController());
+        
+        cont.setStage(stage);
+        cont.initStage(root);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
--- Client/src/view/SignInController.java
+++ Client/src/view/SignInController.java
@@ -6,9 +6,103 @@
 package view;
 
 /**
- *
- * @author javie
+ * Sample Skeleton for 'SignInWindowFXML.fxml' Controller Class
  */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 public class SignInController {
+
+    private Stage stage; //This window Stage
+
+    @FXML // fx:id="signInWindow"
+    private Pane signInWindow; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageLabel"
+    private ImageView imageLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="logInLabel"
+    private Label logInLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernameLabel"
+    private Label usernameLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passwordLabel"
+    private Label passwordLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernameText"
+    private TextField usernameText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passwordText"
+    private PasswordField passwordText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="signInButton"
+    private Button signInButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="signUpAccessLabel"
+    private Label signUpAccessLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="signUpLink"
+    private Hyperlink signUpLink; // Value injected by FXMLLoader
+
+    @FXML // fx:id="showPasswordButton"
+    private ToggleButton showPasswordButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="showPasswordText"
+    private TextField showPasswordText; // Value injected by FXMLLoader
+
+    @FXML
+    public void passwordButtonAction(ActionEvent event) {
+        if (!showPasswordButton.isSelected()) {
+            showPasswordButton.setGraphic(new ImageView("/res/show.png"));
+        }else {
+            showPasswordButton.setGraphic(new ImageView("/res/hide.png"));
+        }
+    }
+
+    @FXML
+    void signInButtonAction(ActionEvent event) {
+
+    }
+
+    public void signUpClicked(ActionEvent event) {
+
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void initStage(Parent root) {
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setOnShowing(this::handleWindowShowing);
+        
+        
+        showPasswordButton.setOnAction(this::passwordButtonAction);
+        signUpLink.setOnAction(this::signUpClicked);
+        signInButton.setOnAction(this::signInButtonAction);
+        
+        stage.show();
+    }
     
+    private void handleWindowShowing(WindowEvent event) {
+        showPasswordText.textProperty().bindBidirectional(passwordText.textProperty());
+        showPasswordButton.setGraphic(new ImageView("/res/show.png"));
+        passwordText.visibleProperty().bind(showPasswordButton.selectedProperty().not());
+        showPasswordText.visibleProperty().bind(showPasswordButton.selectedProperty());
+    }
+
 }
