53379603a26ef29e3f52c59f172384b46ef77b0e
Javier-Fermin <javierferminardila64@gmail.com>
2023-10-18 22:39:35 +0200

    18_10_2023

--- Client/src/view/SignUpController.java
+++ Client/src/view/SignUpController.java
@@ -6,6 +6,9 @@
 package view;
 
 
+import exceptions.BadEmailException;
+import exceptions.BadPasswordException;
+import exceptions.BadPhoneException;
 import exceptions.BadUserException;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -27,61 +30,16 @@ public class SignUpController implements ChangeListener<String>{
     private Stage stage;
     
     @FXML
-    private Label UserLabel;
+    private Label UserLabel,PhoneLabel,MailLabel,AddressLabel,PasswordLabel,ConfirmPasswordLabel,SignInLabel;
 
     @FXML
-    private TextField UserTextField;
+    private TextField UserTextField,PhoneTextField,MailTextField,AddressTextField;
 
     @FXML
-    private Label UserErrorLabel;
-
+    private Label UserErrorLabel,PhoneErrorLabel,MailErrorLabel,AddressErrorLabel,PasswordErrorLabel,ConfirmPasswordErrorLabel;
+    
     @FXML
-    private Label PhoneLabel;
-
-    @FXML
-    private TextField PhoneTextField;
-
-    @FXML
-    private Label PhoneErrorLabel;
-
-    @FXML
-    private Label MailLabel;
-
-    @FXML
-    private TextField MailTextField;
-
-    @FXML
-    private Label MailErrorLabel;
-
-    @FXML
-    private Label AddressLabel;
-
-    @FXML
-    private TextField AddressTextField;
-
-    @FXML
-    private Label AddressErrorLabel;
-
-    @FXML
-    private Label PasswordLabel;
-
-    @FXML
-    private PasswordField PasswordTextField;
-
-    @FXML
-    private Label ConfirmPasswordLabel;
-
-    @FXML
-    private PasswordField ConfirmPasswordTextField;
-
-    @FXML
-    private Label PasswordErrorLabel;
-
-    @FXML
-    private Label ConfirmPasswordErrorLabel;
-
-    @FXML
-    private Label SignInLabel;
+    private PasswordField PasswordTextField,ConfirmPasswordTextField;
 
     @FXML
     private Hyperlink SignInHyperlink;
@@ -118,32 +76,103 @@ public class SignUpController implements ChangeListener<String>{
         ConfirmPasswordTextField.textProperty().addListener(this);
         
         //We set the focus property listeners
-        UserTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if(newValue){
-                    UserLabel.getStyleClass().clear();
-                    UserTextField.getStyleClass().clear();
-                    UserTextField.getStyleClass().addAll("text-field", "text-input", "textFieldWithIcon");
-                    UserErrorLabel.setVisible(false);
-                }else{
-                    try{
+        UserTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if(newValue){
+                UserLabel.getStyleClass().remove("errorLabel");
+                UserTextField.getStyleClass().remove("textFieldError");
+                UserTextField.getStyleClass().add("textFieldWithIcon");
+                UserErrorLabel.setVisible(false);
+            }else{
+                try{
                     if(UserTextField.getText().length() > 500){
                         throw new BadUserException();
                     }
                 }catch(BadUserException e){
                     UserErrorLabel.setVisible(true);
-                    UserLabel.getStyleClass().clear();
                     UserLabel.getStyleClass().add("errorLabel");
-                    UserTextField.getStyleClass().clear();
+                    UserTextField.getStyleClass().remove("textFieldWithIcon");
                     UserTextField.getStyleClass().add("textFieldError");
                 }
-                
-                }
-                
             }
         });
         
+        PhoneTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if(newValue){
+                PhoneLabel.getStyleClass().remove("errorLabel");
+                PhoneTextField.getStyleClass().remove("textFieldError");
+                PhoneTextField.getStyleClass().add("textFieldWithIcon");
+                PhoneErrorLabel.setVisible(false);
+            }else{
+                try{
+                    if(!PhoneTextField.getText().isEmpty() 
+                            && (PhoneTextField.getText().length() > 20 
+                                || !(PhoneTextField.getText().matches("[+][0-9]+") 
+                                    || PhoneTextField.getText().matches("[0-9]+")
+                                    )
+                                )
+                            ){
+                        throw new BadPhoneException();
+                    }
+                }catch(BadPhoneException e){
+                    PhoneErrorLabel.setVisible(true);
+                    PhoneLabel.getStyleClass().add("errorLabel");
+                    PhoneTextField.getStyleClass().remove("textFieldWithIcon");
+                    PhoneTextField.getStyleClass().add("textFieldError");
+                }
+            }
+        });
+        
+        MailTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if(newValue){
+                MailLabel.getStyleClass().remove("errorLabel");
+                MailTextField.getStyleClass().remove("textFieldError");
+                MailTextField.getStyleClass().add("textFieldWithIcon");
+                MailErrorLabel.setVisible(false);
+            }else{
+                try{
+                    if(!MailTextField.getText().isEmpty() 
+                            && (MailTextField.getText().length() > 500
+                                || !MailTextField.getText().matches("[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z0-9]+")
+                                )
+                            ){
+                        throw new BadEmailException();
+                    }
+                }catch(BadEmailException e){
+                    MailErrorLabel.setVisible(true);
+                    MailLabel.getStyleClass().add("errorLabel");
+                    MailTextField.getStyleClass().remove("textFieldWithIcon");
+                    MailTextField.getStyleClass().add("textFieldError");
+                }
+            }
+        });
+        
+        ConfirmPasswordTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if(newValue){
+                ConfirmPasswordLabel.getStyleClass().remove("errorLabel");
+                ConfirmPasswordTextField.getStyleClass().remove("textFieldError");
+                ConfirmPasswordTextField.getStyleClass().add("textFieldWithIcon");
+                ConfirmPasswordErrorLabel.setVisible(false);
+                PasswordLabel.getStyleClass().remove("errorLabel");
+                PasswordTextField.getStyleClass().remove("textFieldError");
+                PasswordTextField.getStyleClass().add("textFieldWithIcon");
+                PasswordErrorLabel.setVisible(false);
+            }else{
+                try{
+                    if(!ConfirmPasswordTextField.getText().equals(PasswordTextField.getText())){
+                        throw new BadPasswordException();
+                    }
+                }catch(BadPasswordException e){
+                    PasswordErrorLabel.setVisible(false);
+                    ConfirmPasswordErrorLabel.setVisible(true);
+                    ConfirmPasswordLabel.getStyleClass().add("errorLabel");
+                    ConfirmPasswordTextField.getStyleClass().remove("textFieldWithIcon");
+                    ConfirmPasswordTextField.getStyleClass().add("textFieldError");
+                    //PasswordLabel.getStyleClass().add("errorLabel");
+                    //PasswordTextField.getStyleClass().remove("textFieldWithIcon");
+                    //PasswordTextField.getStyleClass().add("textFieldError");
+                }
+            }
+        });
         
         stage.setScene(scene);
         stage.show();
