f15cc593976799cee33acc0da63f31be216ed7de
Fran de Ysasi <fdeysasigonzalez@gmail.com>
2023-10-18 14:15:13 +0200

    UPDATE: SignInWindow and MainWindow design

--- Client/src/view/MainWindowController.java
+++ Client/src/view/MainWindowController.java
@@ -5,10 +5,64 @@
  */
 package view;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  *
  * @author javie
  */
 public class MainWindowController {
-    
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Button logOutButton;
+
+    private Stage stage; //This window Stage
+
+    public void setMainStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void initStage(Parent root) {
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        
+        logOutButton.setOnAction(this::logOutAction);
+
+        stage.show();
+
+    }
+
+    @FXML
+    public void logOutAction (ActionEvent event) {
+        try {
+            Stage sStage = new Stage();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SignInWindowFXML.fxml"));
+            Parent root = (Parent) loader.load();
+
+            SignInController cont = ((SignInController) loader.getController());
+
+            cont.setStage(sStage);
+            cont.initStage(root);
+
+            stage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SignInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
--- Client/src/view/SignInController.java
+++ Client/src/view/SignInController.java
@@ -8,8 +8,14 @@ package view;
 /**
  * Sample Skeleton for 'SignInWindowFXML.fxml' Controller Class
  */
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -67,18 +73,51 @@ public class SignInController {
     public void passwordButtonAction(ActionEvent event) {
         if (!showPasswordButton.isSelected()) {
             showPasswordButton.setGraphic(new ImageView("/res/show.png"));
-        }else {
+        } else {
             showPasswordButton.setGraphic(new ImageView("/res/hide.png"));
         }
     }
 
     @FXML
-    void signInButtonAction(ActionEvent event) {
+    public void signInButtonAction(ActionEvent event) {
+        try {
+            //if(){
+            //else{
+            //Abrir ventana
+            Stage sStage = new Stage();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindowFXML.fxml"));
+            Parent root = (Parent) loader.load();
+
+            MainWindowController cont = ((MainWindowController) loader.getController());
+
+            cont.setMainStage(sStage);
+            cont.initStage(root);
+
+            stage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SignInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
+    @FXML
     public void signUpClicked(ActionEvent event) {
+        try {
+            Stage sStage = new Stage();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SignUP.fxml"));
+            Parent root = (Parent) loader.load();
+
+            SignUpController cont = ((SignUpController) loader.getController());
+
+            cont.setSignUpStage(sStage);
+            cont.initStage(root);
+
+            stage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SignInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void setStage(Stage stage) {
@@ -89,20 +128,31 @@ public class SignInController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setOnShowing(this::handleWindowShowing);
-        
-        
+        stage.setResizable(false);
+
         showPasswordButton.setOnAction(this::passwordButtonAction);
         signUpLink.setOnAction(this::signUpClicked);
         signInButton.setOnAction(this::signInButtonAction);
-        
+
         stage.show();
     }
-    
+
     private void handleWindowShowing(WindowEvent event) {
         showPasswordText.textProperty().bindBidirectional(passwordText.textProperty());
         showPasswordButton.setGraphic(new ImageView("/res/show.png"));
         passwordText.visibleProperty().bind(showPasswordButton.selectedProperty().not());
         showPasswordText.visibleProperty().bind(showPasswordButton.selectedProperty());
+    }
+
+    private static final String EMAIL_PATTERN
+            = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    public static boolean isValid(final String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
 }
