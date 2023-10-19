3ec27f76a8c7cd2d94619b7926bcc9dd55241ce9
Javier-Fermin <javierferminardila64@gmail.com>
2023-10-19 11:57:34 +0200

    19_10_2023

--- Server/src/threads/Worker.java
+++ Server/src/threads/Worker.java
@@ -5,12 +5,33 @@
  */
 package threads;
 
+import java.net.Socket;
+import model.RegistrableFactory;
+import src.Message;
+
 /**
  *
  * @author javie
  */
 public class Worker extends Thread{
+    
+    private final RegistrableFactory factory;
+    private final Socket socket;
+    private final Message message;
+
+    public Worker(RegistrableFactory factory, Socket socket, Message message) {
+        this.factory = factory;
+        this.socket = socket;
+        this.message = message;
+    }
+    
     @Override
-    public void run(){    
+    public void run(){
+        switch (message.getMessageType()){
+            case SIGNIN_REQUEST:
+                break;
+            case SIGNUP_REQUEST:
+                break;
+        }
     }
 }
