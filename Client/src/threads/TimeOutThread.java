2975ebb9959b36d6f3cb0b055f034987eb8cd0bb
2dam <2dam@TartangaLH.eus>
2023-10-20 09:32:17 +0200

    TimeOutThread first version

--- Client/src/threads/TimeOutThread.java
+++ Client/src/threads/TimeOutThread.java
@@ -5,12 +5,28 @@
  */
 package threads;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import src.TimeOutException;
+
 /**
  *
  * @author javie
  */
-public class TimeOutThread extends Thread{
+public class TimeOutThread extends Thread {
+
     @Override
-    public void run(){    
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(30);
+            try {
+                throw new TimeOutException();
+            } catch (TimeOutException e) {
+                Logger.getLogger(TimeOutThread.class.getName()).log(Level.FINE, null, e.getMessage());
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TimeOutThread.class.getName()).log(Level.FINE, null, "All fine");
+        }
     }
 }
