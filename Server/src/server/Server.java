d79e02b8c5d93942655262d4c1aa4341717b4b17
PerezImanol <imanolperezmu@gmail.com>
2023-10-20 09:52:36 +0200

    20/10/23-9:50

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,21 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
 
-/**
- *
- * @author javie
- */
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import model.RegistrableFactory;
+import threads.Worker;
+
 public class Server {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private final int PUERTO = 5000;
+    private final String clave = "abc";
+    private final RegistrableFactory factory = new RegistrableFactory();
+
+    public void iniciar() {
+        ServerSocket servidor = null;
+        Socket cliente = null;
+        ObjectInputStream entrada = null;
+        ObjectOutputStream salida = null;
+        try {
+            servidor = new ServerSocket(PUERTO);
+            cliente = servidor.accept();
+            Worker worker = new Worker(factory, cliente);
+            
+        } catch (IOException e) {
+            
+        } catch (Exception e) {
+            
+        } finally {
+            try {
+                if (servidor != null) {
+                    servidor.close();
+                }
+                if (cliente != null) {
+                    cliente.close();
+                }
+                if (entrada != null) {
+                    entrada.close();
+                }
+                if (salida != null) {
+                    salida.close();
+                }
+            } catch (IOException e) {
+                
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+        Server s1 = new Server();
+        s1.iniciar();
+    }
 }
