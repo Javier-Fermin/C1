8fe560604fc1f90cb96efba1f67e730abd3b4a54
Javier-Fermin <javierferminardila64@gmail.com>
2023-10-26 22:37:21 +0200

    26_10_2023

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -9,59 +11,90 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.RegistrableFactory;
-import threads.WaitToStopThread;
+//import threads.WaitToStopThread;
 import threads.Worker;
 
 /**
- * This class is the responsible of initialize the server with an open connection
- * and make aviable to exit the application
+ * This class is the responsible of initialize the server with an open
+ * connection and make aviable to exit the application
+ *
  * @author Imanol
  */
 public class Server {
+
     private final String PUERTO = ResourceBundle.getBundle("./resources/Properties.properties").getString("PORT");
     private final String MAX_THREADS = ResourceBundle.getBundle("./resources/Properties.properties").getString("MAX_THREADS");
     private final RegistrableFactory factory = new RegistrableFactory();
+    
+    private final Integer LOCAL_PORT_SSH_TUNNEL = Integer.parseInt(ResourceBundle.getBundle("./resources/Properties.properties").getString("LOCAL_PORT_SSH_TUNNEL"));
+    private final Integer REMOTE_PORT_SSH_TUNNEL = Integer.parseInt(ResourceBundle.getBundle("./resources/Properties.properties").getString("REMOTE_PORT_SSH_TUNNEL"));
+    private final String REMOTE_IP_SSH_TUNNEL = ResourceBundle.getBundle("./resources/Properties.properties").getString("REMOTE_IP_SSH_TUNNEL");
+    private final Integer PORT_SSH_CONNECTION = Integer.parseInt(ResourceBundle.getBundle("./resources/Properties.properties").getString("PORT_SSH_CONNECTION"));
+    private final String IP_SSH_CONNECTION = ResourceBundle.getBundle("./resources/Properties.properties").getString("IP_SSH_CONNECTION");
+    private final String USER_SSH_CONNECTION = ResourceBundle.getBundle("./resources/Properties.properties").getString("USER_SSH_CONNECTION");
+    private final String PASSWD_SSH_CONNECTION = ResourceBundle.getBundle("./resources/Properties.properties").getString("PASSWD_SSH_CONNECTION");
+
+    
     private final Pool pool = new Pool();
-    private Integer threads = 0;
-   
+    private static Integer threads = 0;
+
     public void iniciar() {
         ServerSocket server = null;
         Socket client = null;
-       while(true){
-           try {
-            // creates a server socken in the given port
-            server = new ServerSocket(Integer.parseInt(PUERTO));
-            System.out.println("Waiting the client ...");
-            // waits until the client connects to the server
-            client = server.accept();
-            System.out.println("Client connected!");
-            /* then it creates a worker with the parameters of the factory 
-            * to process the message
-            */
-            if(threads < Integer.parseInt(MAX_THREADS)){
-                Woker worker = new Worker(factory, client, pool);
-                threads++;
+        try {
+            Session session = new JSch().getSession(
+                    USER_SSH_CONNECTION,
+                    IP_SSH_CONNECTION,
+                    PORT_SSH_CONNECTION);
+            session.setPassword(PASSWD_SSH_CONNECTION);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            session.setPortForwardingL(
+                    LOCAL_PORT_SSH_TUNNEL,
+                    REMOTE_IP_SSH_TUNNEL,
+                    REMOTE_PORT_SSH_TUNNEL);
+            while (true) {
+                // creates a server socken in the given port
+                server = new ServerSocket(Integer.parseInt(PUERTO));
+                System.out.println("Waiting the client ...");
+                // waits until the client connects to the server
+                client = server.accept();
+                System.out.println("Client connected!");
+                /* then it creates a worker with the parameters of the factory 
+                 * to process the message
+                 */
+                if (threads < Integer.parseInt(MAX_THREADS)) {
+                    Worker worker = new Worker(factory, client, pool);
+                    worker.run();
+                    threads++;
+                }
             }
-            
-            
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         } finally {
             try {
-                if (server != null)
+                if (server != null) {
                     server.close();
-                if (client != null)
+                }
+                if (client != null) {
                     client.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
             System.out.println("Fin servidor");
         }
     }
-       }
-        
+
+    public static synchronized Integer getThreads() {
+        return threads;
+    }
+
+    public static synchronized void setThreads(Integer threads) {
+        Server.threads = threads;
+    }
 
     public static void main(String[] args) {
         Server s1 = new Server();
